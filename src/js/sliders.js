4386420ac9c482fb6c4446279c6fb713a7032b5e
Darya Zhihareva <zhihareva.d@mail.ru>
2021-04-06 14:40:50 +0700

    Made basic structure of page

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,117 +1,32 @@
 const sliders = () => {
   const Swiper = window.Swiper;
 
-  // Adv slider
-  const advantages = document.querySelector(".js-advantages-slider");
+  // Slider popular
+  const popular = document.querySelector(".js-slider-popular");
 
-  if (advantages) {
-    const mySwiper = new Swiper(".js-advantages-slider.swiper-container", {
+  if (popular) {
+    const mySwiper = new Swiper(".js-slider-popular .swiper-container", {
       direction: "horizontal",
       slidesPerView: 1,
       spaceBetween: 20,
       speed: 400,
       navigation: {
-        nextEl: ".js-advantages-slider .swiper-button-next",
-        prevEl: ".js-advantages-slider .swiper-button-prev",
+        nextEl: ".js-slider-popular .swiper-button-next",
+        prevEl: ".js-slider-popular .swiper-button-prev",
       },
-    });
-  }
-
-  // Photos slider
-  const photos = document.querySelector(".js-photos-slider");
-
-  if (photos) {
-    const mySwiper = new Swiper(".js-photos-slider.swiper-container", {
-      direction: "horizontal",
-      slidesPerView: 1,
-      spaceBetween: 20,
-      speed: 400,
-      loop: true,
-      centeredSlides: false,
       breakpoints: {
-        767: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-          centeredSlides: true,
-        },
-      },
-      navigation: {
-        nextEl: ".js-photos-slider .swiper-button-next",
-        prevEl: ".js-photos-slider .swiper-button-prev",
-      },
-      pagination: {
-        el: '.js-photos-slider .swiper-pagination',
-        clickable: true,
-      },
-    });
-  }
-
-
-  // Reviews slider
-  const reviews = document.querySelector(".js-reviews-slider");
-
-  if (reviews) {
-    const mySwiper = new Swiper(".js-reviews-slider.swiper-container", {
-      direction: "horizontal",
-      slidesPerView: 1,
-      spaceBetween: 20,
-      speed: 400,
-      loop: true,
-      centeredSlides: true,
-      breakpoints: {
-        680: {
+        460: {
           slidesPerView: 2,
           spaceBetween: 15,
-          centeredSlides: false,
         },
-        767: {
-          slidesPerView: 3,
-          spaceBetween: 15,
-          centeredSlides: true,
-        },
-        991: {
-          slidesPerView: 3,
-          spaceBetween: 120,
-          centeredSlides: true,
-        },
-      },
-      navigation: {
-        nextEl: ".js-reviews-slider .swiper-button-next",
-        prevEl: ".js-reviews-slider .swiper-button-prev",
-      },
-    });
-  }
-
-  // Sertificates slider
-  const sertificates = document.querySelector(".js-sertificates-slider");
-
-  if (sertificates) {
-    const mySwiper = new Swiper(".js-sertificates-slider.swiper-container", {
-      direction: "horizontal",
-      slidesPerView: 1,
-      spaceBetween: 20,
-      speed: 400,
-      loop: true,
-      centeredSlides: true,
-      breakpoints: {
-        500: {
-          slidesPerView: 2,
-          spaceBetween: 15,
-          centeredSlides: false,
-        },
-        680: {
+        660: {
           slidesPerView: 3,
           spaceBetween: 15,
         },
-        991: {
-          slidesPerView: 3,
-          spaceBetween: 120,
-          centeredSlides: true,
+        960: {
+          slidesPerView: 4,
+          spaceBetween: 8,
         },
-      },
-      navigation: {
-        nextEl: ".js-sertificates-slider .swiper-button-next",
-        prevEl: ".js-sertificates-slider .swiper-button-prev",
       },
     });
   }
