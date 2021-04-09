3f8d6b58d3b5d877131d939787c283032e7672eb
Darya Zhihareva <zhihareva.d@mail.ru>
2021-04-09 10:52:48 +0700

    Made deck menu and mob header

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import number from './number';
 import btnUp from './btn-up';
 import goodQuantity from './good-quantity';
 import footerForm from './footer-form';
+import deskMenu from './desk-menu';
 
 class App {
   static init() {
@@ -21,6 +22,7 @@ class App {
     btnUp();
     goodQuantity();
     footerForm();
+    deskMenu();
   }
 }
 
